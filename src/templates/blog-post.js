99487bae033e3bb46ed58e1eb64025ebc8edbbd2
Matthew Reishus <mreishus@users.noreply.github.com>
2019-05-19 17:13:18 -0500

    Add MathJax

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,64 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import Helmet from "react-helmet"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
 
+function waitForGlobal(name, timeout = 300) {
+  return new Promise((resolve, reject) => {
+    let waited = 0
+
+    function wait(interval) {
+      setTimeout(() => {
+        waited += interval
+        // some logic to check if script is loaded
+        // usually it something global in window object
+        if (window[name] !== undefined) {
+          return resolve()
+        }
+        if (waited >= timeout * 1000) {
+          return reject({ message: "Timeout" })
+        }
+        wait(interval * 2)
+      }, interval)
+    }
+
+    wait(30)
+  })
+}
+
+/* eslint-disable no-restricted-globals */
 class BlogPostTemplate extends React.Component {
+  componentDidMount() {
+    waitForGlobal("MathJax").then(() => {
+      top.MathJax.Hub.Config({
+        tex2jax: {
+          inlineMath: [["$", "$"], ["\\(", "\\)"]],
+          displayMath: [["$$", "$$"], ["[", "]"]],
+          processEscapes: true,
+          processEnvironments: true,
+          skipTags: ["script", "noscript", "style", "textarea", "pre"],
+          TeX: {
+            equationNumbers: { autoNumber: "AMS" },
+            extensions: ["AMSmath.js", "AMSsymbols.js"],
+          },
+        },
+      })
+    })
+    if (top.MathJax != null) {
+      top.MathJax.Hub.Queue(["Typeset", top.MathJax.Hub])
+    }
+  }
+
+  componentDidUpdate() {
+    if (top.MathJax != null) {
+      top.MathJax.Hub.Queue(["Typeset", top.MathJax.Hub])
+    }
+  }
+
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
@@ -18,6 +70,13 @@ class BlogPostTemplate extends React.Component {
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
+        <Helmet>
+          <script
+            type="text/javascript"
+            src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+            async
+          />
+        </Helmet>
         <h1>{post.frontmatter.title}</h1>
         <p
           style={{
