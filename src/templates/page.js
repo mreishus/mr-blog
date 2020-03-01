079b63a6fbf11a3e68ae0021f7ae37238706f87f
Matthew Reishus <mreishus@users.noreply.github.com>
2020-03-01 08:57:20 -0600

    Fix

--- src/templates/page.js
+++ src/templates/page.js
@@ -29,7 +29,9 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
-        author
+        author {
+          name
+        }
       }
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
