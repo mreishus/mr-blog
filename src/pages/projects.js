b43210926acf59ef0e1a105337b6ee6e54c87a64
Matthew Reishus <mreishus@users.noreply.github.com>
2020-03-16 19:42:34 -0500

    Update projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,15 +5,18 @@ class Projects extends React.Component {
   render() {
     // const { data } = this.props
     // const siteTitle = data.site.siteMetadata.title
-    const siteTitle = "Projects"
+    //const siteTitle = "Projects"
+    const siteTitle = null
     return (
       <Layout location={this.props.location} title={siteTitle}>
+        <h1>Personal Projects</h1>
+        <p>These are small projects I've made by myself.</p>
         <h2>
-          <a href="https://sbin.top/f/">Sbin</a>
+          <a href="https://sbin.top/">Sbin</a>
         </h2>
         <p>
-          <a href="https://sbin.top/f/">Sbin</a> is an encrypted online paste
-          bin, allowing users to share snippets of text by passing around a URL.
+          <a href="https://sbin.top/">Sbin</a> is an encrypted online paste bin,
+          allowing users to share snippets of text by passing around a URL.
           Syntax highlighting is provided, but the language must be chosen when
           creating a paste. All pastes have an expire date when they are
           automatically purged.
@@ -28,6 +31,101 @@ class Projects extends React.Component {
         <p>
           <a href="https://github.com/mreishus/sbin">Github source.</a> Sbin is
           built using Elixir, Phoenix, React, and Postgres.
+        </p>
+        <h2>
+          <a href="https://demonspirit.xyz/">Demon Spirit</a>
+        </h2>
+        <p>
+          <a href="https://demonspirit.xyz/">Demon Spirit</a> is an online board
+          game. It's an abstract game played on a 5x5 grid, based on the
+          commerical game Onitama. Users may create rooms and play against
+          another users or against a computer AI with adjustable difficulty. No
+          registration is required.
+        </p>
+        <p>
+          <a href="https://github.com/mreishus/demon_spirit_umbrella">
+            Github Source.
+          </a>{" "}
+          Demon Spirit is built using Elixir, Phoenix, and Phoenix Live View.
+          Realtime updates are pushed through phoenix channels using websockets.
+        </p>
+        <h2>
+          <a href="https://starspades.com/">StarSpades</a>
+        </h2>
+        <p>
+          <a href="https://starspades.com/">StarSpades</a> is an online card
+          game. It's an implementation of the classic game spades. Users may
+          create rooms and play against each other or AI bots. Registration is
+          required.
+        </p>
+        <p>
+          <a href="https://github.com/mreishus/spades">Github Source.</a> Demon
+          Spirit is built using Elixir, Phoenix, and React. Realtime updates are
+          pushed through phoenix channels using websockets.
+        </p>
+        <h2>
+          <a href="https://snes.matthewreishus.com/">SNES Classifier</a>
+        </h2>
+        <p>
+          This is a model built using a neural network. I've trained it to
+          classify screenshots of SNES games. Upload a screenshot of any SNES
+          game; the model will tell you which game the screenshot came from.
+        </p>
+        <p>
+          <a href="https://github.com/mreishus/snes_classifier">
+            Github Source.
+          </a>{" "}
+          See writeup{" "}
+          <a href="https://matthewreishus.com/2019-06-03-snes-classifier/">
+            here.
+          </a>
+        </p>
+        <h2>
+          <a href="https://github.com/mreishus/aoc/">
+            My Advent of Code Solutions
+          </a>
+        </h2>
+        <p>
+          I've participated in the Programming event,{" "}
+          <a href="https://adventofcode.com">Advent of Code</a> and solved all
+          problems in all years, 2015-2019. I did not make the Advent of Code
+          site, I'm just a participant.
+        </p>
+        <p>
+          <a href="https://github.com/mreishus/aoc/">
+            Solutions on Github with writeups for most problems.
+          </a>
+        </p>
+        <h1>Professional Projects</h1>
+        <p>
+          Projects I've contributed to, in a team context, at full-time jobs.
+        </p>
+        <h2>Experian</h2>
+        <p>
+          2016-2019
+          <br />
+          <a href="https://omniactivation.experian.com/">Audience Engine</a>
+          <br />
+          Data Engine
+        </p>
+        <h2>Allied Health Media</h2>
+        <p>
+          2011-2015 <br />
+          <a href="https://www.speechpathology.com/">SpeechPathology</a>
+          <br />
+          <a href="https://www.audiologyonline.com/">Audiology Online</a>
+          <br />
+          <a href="https://www.physicaltherapy.com/">Physical Therapy</a>
+          <br />
+          <a href="https://www.occupationaltherapy.com/">
+            Occupational Therapy
+          </a>
+        </p>
+        <h2>HRsmart</h2>
+        <p>
+          2007-2011
+          <br />
+          300+ TMS Job Boards (now defunct)
         </p>
       </Layout>
     )
